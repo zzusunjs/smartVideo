a6484e6aa0886aa98e7ab782f022c269736c5687
zzusunjs@qq.com <zzusunjs@qq.com>
2024-05-24 23:57:39 +0800

    feat: init typescript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+/** @file index.ts the entrance */
+const test = (n) => {
+    console.log(`hello ${n}`);
+};
+test('world');
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/** @file index.ts the entrance */
+
+const test = (n : string) => {
+  console.log(`hello ${n}`);
+}
+
+
+test('world');
